660830cb70fb2ee69a6db122a9d41c674830d1eb
AhmadSater <ahmadabdulsater@hotmail.com>
2020-03-27 17:09:44 +0100

    Initialization
    Initiating repo, with instructions from Isak Näslund

---
+++ FEM_Engine/Query/Area.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.FEM;
+
+namespace BH.Engine.FEM
+{
+    public static partial class Query
+    {
+        public static double Area(this Rectangle rectangle)
+        {
+            return 0;
+        }
+    }   
+}
---
+++ FEM_oM/Rectangle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Base;
+
+namespace BH.oM.FEM
+{
+    public class Rectangle : BHoMObject
+    {
+        public double Height { get; set; }
+        public double Width { get; set; }
+    }
+}
