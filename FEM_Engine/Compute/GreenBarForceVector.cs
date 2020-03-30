3fdb2aa6cb2dba34e143c892ff440194b5ad8eec
AhmadSater <ahmadabdulsater@hotmail.com>
2020-03-30 13:41:16 +0200

    Add class for Green Bar Force Element

--- FEM_Engine/Compute/GreenBarForceVector.cs
+++ FEM_Engine/Compute/GreenBarForceVector.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BH.oM.Structure.Elements;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Double;
 
-namespace BH.Engine.FEM.Compute
+namespace BH.Engine.FEM
 {
-    class GreenBarForceVector
+    public static partial class Compute
     {
+        public static Matrix<double> GreenBarForceVector(Bar aBar, double N, Vector<double> ed)
+        {
+            double[,] ecArray = { { aBar.StartNode.Position.X, aBar.EndNode.Position.X }, { aBar.StartNode.Position.Y, aBar.EndNode.Position.Y }, { aBar.StartNode.Position.Z, aBar.EndNode.Position.Z } };
+            Matrix<double> ec = DenseMatrix.OfArray(ecArray);
+
+            Vector<double> xo = ec.Column(1) - ec.Column(0);
+
+            Vector<double> edStart = ed.SubVector(0, 3);
+            Vector<double> edEnd = ed.SubVector(3, 3);
+
+            Vector<double> u = edEnd - edStart;
+            Vector<double> x = xo + u;
+
+            double deltaX = aBar.EndNode.Position.X - aBar.StartNode.Position.X;
+            double deltaY = aBar.EndNode.Position.Y - aBar.StartNode.Position.Y;
+            double deltaZ = aBar.EndNode.Position.Z - aBar.StartNode.Position.Z;
+
+            double lo = Math.Sqrt(Math.Pow(deltaX, 2) + Math.Pow(deltaY, 2) + Math.Pow(deltaZ, 2));
+
+            Matrix<double> ef = (N / lo) * (-1 * x).ToColumnMatrix().Stack(x.ToColumnMatrix());
+            return ef;
+        }
     }
 }
--- FEM_Engine/Compute/GreenBarSitffnessMatrix.cs
+++ FEM_Engine/Compute/GreenBarSitffnessMatrix.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BH.Engine.FEM.Compute
+namespace BH.Engine.FEM
 {
-    class GreenBarSitffnessMatrix
+    public static partial class Compute
     {
     }
 }
