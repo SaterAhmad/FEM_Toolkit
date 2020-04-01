5993e87957d94bc87bd872e1f4803e2635def9d5
AhmadSater <ahmadabdulsater@hotmail.com>
2020-04-01 14:21:04 +0200

    Adding FEMResult Object & outputting BMoM Results

--- FEM_Engine/Compute/Solve.cs
+++ FEM_Engine/Compute/Solve.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BH.oM.FEM;
 using BH.oM.Structure.Elements;
 using BH.oM.Structure.Loads;
+using BH.oM.Structure.Results;
 using MathNet.Numerics.LinearAlgebra;
 using MathNet.Numerics.LinearAlgebra.Double;
 
@@ -12,7 +14,7 @@ namespace BH.Engine.FEM
 {
     public static partial class Compute
     {
-        public static Vector<double> Solve(List<Bar> bars, List<Node> constraints, List<PointLoad> loads, int maxIter, double tol)
+        public static List<FEMResult> Solve(List<Bar> bars, List<Node> constraints, List<PointLoad> loads, int maxIter, double tol)
         {
             int nEL = bars.Count;
 
@@ -112,7 +114,67 @@ namespace BH.Engine.FEM
                     break;
                 }
             }
-            return u;
+
+            List<FEMResult> outResults = new List<FEMResult>();
+
+            for (int i = 0; i < nEL; i++)
+            {
+                FEMResult outResult1 = new FEMResult();
+
+                BarDeformation barDef1 = new BarDeformation();
+                BarDisplacement barDisp1 = new BarDisplacement();
+                BarForce barForce1 = new BarForce();
+                BarStrain barStrain1 = new BarStrain();
+                BarStress barStress1 = new BarStress();
+
+                barDisp1.UX = ed[i, 0];
+                barDisp1.UY = ed[i, 1];
+                barDisp1.UZ = ed[i, 2];
+                barDisp1.ObjectId = bars[i].Name;
+                barDisp1.ResultCase = loads[0].Loadcase.Number;
+                barDisp1.Position = 0;
+                barDisp1.Divisions = 2;
+
+                barForce1.FX = es[i];
+                barForce1.ObjectId = bars[i].Name;
+                barForce1.ResultCase = loads[0].Loadcase.Number;
+                barForce1.Position = 0;
+                barForce1.Divisions = 2;
+
+                outResult1.barDisplacement = barDisp1;
+                outResult1.barForce = barForce1;
+
+                FEMResult outResult2 = new FEMResult();
+
+                BarDeformation barDef2 = new BarDeformation();
+                BarDisplacement barDisp2 = new BarDisplacement();
+                BarForce barForce2 = new BarForce();
+                BarStrain barStrain2 = new BarStrain();
+                BarStress barStress2 = new BarStress();
+
+                barDisp2.UX = ed[i, 3];
+                barDisp2.UY = ed[i, 4];
+                barDisp2.UZ = ed[i, 5];
+                barDisp2.ObjectId = bars[i].Name;
+                barDisp2.ResultCase = loads[0].Loadcase.Number;
+                barDisp2.Position = 1;
+                barDisp2.Divisions = 2;
+
+                barForce2.FX = es[i];
+                barForce2.ObjectId = bars[i].Name;
+                barForce2.ResultCase = loads[0].Loadcase.Number;
+                barForce2.Position = 1;
+                barForce2.Divisions = 2;
+
+                outResult2.barDisplacement = barDisp2;
+                outResult2.barForce = barForce2;
+               
+                outResults.Add(outResult1);
+                outResults.Add(outResult2);
+            }
+
+            //return u;
+            return outResults;          
         }
     }
 }
